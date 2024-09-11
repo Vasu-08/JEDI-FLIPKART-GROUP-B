77a88403547c40c93be516c79e417a925ad74b3f
Ashutosh Saini <rushbuket@gmail.com>
2024-09-11 11:25:09 +0530

    refactored POS repo

--- src/com/flipkart/business/interfaces/IFlipFitSlots.java
+++
@@ -1,9 +0,0 @@
-package com.flipkart.business.interfaces;
-
-import com.flipkart.bean.FlipFitSlots;
-import com.flipkart.dao.FlipFitSlotDAOImpl;
-
-public interface IFlipFitSlots {
-    public boolean updateAvailability(FlipFitSlots flipFitSlots);
-    public void getSlotDetails();
-}
